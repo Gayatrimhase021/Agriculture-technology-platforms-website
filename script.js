3cd1f63eb88bc8f4e6604e5b4f9f60211fd89bb9
Gayatrimhase021 <gayatrimhase879@gmail.com>
2026-02-23 13:52:50 +0530

    added some info

--- script.js
+++
@@ -1,6 +0,0 @@
-function showmessage(){
-    alert("welcome  to  farmer web portal");
-}
-function showPrice(cropName, price) {
-    alert("Today's market price of " + cropName + " is ₹" + price + " per quintal.");
-}
